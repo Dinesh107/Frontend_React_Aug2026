81452e36c20966ff891f556592712d7dfc62322a
Dinesh107 <dineshjr107@gmail.com>
2026-08-11 19:06:04 +0530

    9th commit

---
+++ index.js
@@ -0,0 +1,28 @@
+// printf();
+// cout()
+// System.out.println()
+// console.writeLine();
+// print("", 1, true or false, 0.4, '')
+
+
+// console.log("Welcome to javascript world");
+// console.log("I lkie js");
+
+// window.alert("This is javascipt");
+// window.prompt("");
+
+// document.getElementById("myH1").textContent = "Welcome to javascript";
+
+// document.getElementById("myP").textContent = "i like js";
+
+
+let firstName = "Ganesh";
+let favNum1 = 10.7;
+let favNum2 = 10;
+let isStudent = true;
+
+console.log(typeof firstName);
+console.log(typeof favNum1);
+console.log(typeof favNum2);
+console.log(isStudent);
+console.log(typeof isStudent);
