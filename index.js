ad4693c9682f9014a0a616eb44a319d011e9a768
Dinesh107 <dineshjr107@gmail.com>
2026-08-12 19:04:56 +0530

    10th commit

--- index.js
+++ index.js
@@ -16,13 +16,128 @@
 // document.getElementById("myP").textContent = "i like js";
 
 
-let firstName = "Ganesh";
-let favNum1 = 10.7;
-let favNum2 = 10;
-let isStudent = true;
+// let firstName = "Ganesh";
+// let favNum1 = 10.7;
+// let favNum2 = 10;
+// let isStudent = true;
 
-console.log(typeof firstName);
-console.log(typeof favNum1);
-console.log(typeof favNum2);
-console.log(isStudent);
-console.log(typeof isStudent);
+// console.log(typeof firstName);
+// console.log(typeof favNum1);
+// console.log(typeof favNum2);
+// console.log(isStudent);
+// console.log(typeof isStudent);
+
+// let empCount = 31;
+
+//           31       1 = 32
+// empCount = empCount + 1;
+// empCount = empCount - 1;
+// empCount = empCount * 2;
+// empCount = empCount / 2;
+// empCount = empCount ** 2;
+// empCount = empCount % 2;
+
+
+// let extraEmployee = empCount  % 3;
+
+// agumeneted assigment operator 
+// empCount += 1;
+// empCount -= 1;
+// empCount *= 1;
+// empCount /= 1;
+// empCount %= 1;
+// console.log(empCount);
+// console.log(extraEmployee);
+
+// let empCount = 31;
+
+// empCount++;
+// empCount--;
+
+// console.log(empCount);
+
+
+// let result = 1 + 2 * 3 + 4 ** 2;
+
+// let result = 1 + 2 * 3 + 16;
+
+// let result = 1 + 6 + 16;
+
+// let result = 7 + 16;
+
+// let result = 23;
+
+// result = 12 % 5 + 8 / 2;
+// let result = 2 + 8 / 2;
+// let result = 2 + 4;
+// let result = 6;
+
+// let result = 6 / 2 ** (2 * 5);
+
+// console.log(result);
+
+
+
+
+// let username;
+
+// username = window.prompt("what is your username? ");
+
+// console.log(username);
+
+// let username;
+// document.getElementById("mySubmit").onclick = function() {
+//      username = document.getElementById("myText").value;
+//      username = document.getElementById("myH1").textContent = `Welcome ${username}`;
+//      console.log(username);
+// }
+
+
+
+// let username = "siva";
+
+// console.log(`hello ${username}`);
+
+// let age = window.prompt("How old are you");
+// age = Number(age);
+// age += 1;
+// console.log(age);
+
+// console.log(age, typeof age);
+
+// let x = "veg rice";
+// let y = "veg rice";
+// let z = "veg rice";
+
+// let x = "1ertyuio11";
+// let y = "0";
+// let z = "0";
+
+// let x = "";
+// let y = "";
+// let z = "";
+
+// let x;
+// let y;
+// let z;
+
+// x = Number(x);
+// y = String(y);
+// z = Boolean(z);
+
+// console.log(x, typeof x);
+// console.log(y, typeof y);
+// console.log(z, typeof z);
+
+
+// const PI = 3.14159;
+// let radius;
+// let circumference;
+// PI = 6789.3;
+// radius = window.prompt(`Enter the radius of circle`);
+// radius = Number(radius);
+// circumference = 2 * PI * radius;
+
+// console.log(circumference);
+
+
